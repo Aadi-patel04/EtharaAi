a9d2de3e5c8599bbf2bf6cf0a07282af6b678d54
Aadi Patel <aadipatel0042@gmail.com>
2026-05-01 16:39:22 +0530

    update server

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("DB Connected");
 
-    app.listen(PORT, () => {
+    app.listen(PORT,"0.0.0.0", () => {
       console.log(`Server running on ${PORT}`);
     });
 
